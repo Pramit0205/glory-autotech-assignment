7d7b3e6c0f29c7fab99fd8bafc2caa289b8b7ad3
Pramit0205 <pramitchoudhury0205@gmail.com>
2022-10-21 11:12:51 +0530

    done

--- src/route/route.js
+++ src/route/route.js
@@ -12,9 +12,7 @@ router.post('/firstDay',firstDay)
 router.put("/updateAttendence/:employId",updateAttendence)
 
 router.get('/getEmployDetails/:date',getEmployDetails)
-// router.post('/attendance/:employId',attendance)
-// router.put('/repeateAttendence/:employId',repeateAttendence)
-// router.put('/updateEmploy/:employId',updateEmploy)
+
 
 
 module.exports = router
